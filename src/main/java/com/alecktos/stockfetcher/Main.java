82a07b6b1e12f4a7cee074d6c6116ae95e7ef92f
Alecktos <persson33@gmail.com>
2017-06-19 21:08:31 +0200

    removed unnecessary check

--- src/main/java/com/alecktos/stockfetcher/Main.java
+++ src/main/java/com/alecktos/stockfetcher/Main.java
@@ -10,7 +10,7 @@ import com.google.inject.Module;
 public class Main {
 
 	public static void main(String[] args) {
-		String mode = args.length > 0 ? args[0] : "emulation";
+		String mode = args[0];
 		String stockFilePath = args[1];
 		String emailConfigPath = args[2];
 		String openingTime = args[3];
