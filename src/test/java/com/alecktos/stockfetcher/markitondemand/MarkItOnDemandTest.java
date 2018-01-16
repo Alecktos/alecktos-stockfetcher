d2bdb1fa7ac549d84b40ce113ee93125c5e2c6a9
Alecktos <persson33@gmail.com>
2018-01-16 12:21:51 +0100

    adding test for fetching price

--- src/test/java/com/alecktos/stockfetcher/markitondemand/MarkItOnDemandTest.java
+++ src/test/java/com/alecktos/stockfetcher/markitondemand/MarkItOnDemandTest.java
@@ -1,6 +1,18 @@
 package com.alecktos.stockfetcher.markitondemand;
 
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 public class MarkItOnDemandTest {
+
+	@Test
+	public void testFetchingPrice() {
+		MarkItOnDemand markItOnDemand = new MarkItOnDemand();
+		double currentStockPrice = markItOnDemand.getCurrentStockPrice();
+		assertTrue(currentStockPrice > 0);
+	}
 
 }
